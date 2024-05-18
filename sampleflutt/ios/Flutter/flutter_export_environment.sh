b3505a7e774bb1cd015ebe0cb7c4b5409823e6f4
christinekeni <christinekeni@strathmore.edu>
2024-05-18 22:54:15 +0300

    random changes i had

--- sampleflutt/ios/Flutter/flutter_export_environment.sh
+++ sampleflutt/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\flutter_windows_3.19.6-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\New folder\htdocs\Strathmover\sampleflutt"
+export "FLUTTER_ROOT=C:\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Program Files\XAMPP\htdocs\Strathmover\sampleflutt"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
