42708b3095e987cc8c8149f02c6039a0671ff3e1
benweru <ben.maina@strathmore.edu>
2024-05-21 13:06:38 +0300

    Added welcome screen and button themes

---
+++ bus_app/lib/src/utils/theme/widgets_theme/elevated_button_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+/* -- Light & Dark Elevated Button Themes -- */
+  class TElevatedButtonTheme {
+    TElevatedButtonTheme._(); //To avoid creating instances
+    /* -- Light Theme -- */
+    static final lightElevatedButtonTheme = ElevatedButtonThemeData();
+    /* Dark Theme -- */
+    static final darkElevatedButtonTheme = ElevatedButtonThemeData();
+  }
