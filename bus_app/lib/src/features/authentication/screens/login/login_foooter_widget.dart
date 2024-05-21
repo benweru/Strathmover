1236ae8e9849c303d0ad36fb992c42d64a779879
benweru <ben.maina@strathmore.edu>
2024-05-21 11:07:41 +0300

    Added Login UI

---
+++ bus_app/lib/src/features/authentication/screens/login/login_foooter_widget.dart
@@ -0,0 +1,40 @@
+import 'package:bus_app/src/constants/sizes.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class LoginFooterWidget extends StatelessWidget {
+  const LoginFooterWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const Text("OR"),
+        const SizedBox(height: tFormHeight - 20),
+        SizedBox(
+          width: double.infinity,
+          child: OutlinedButton.icon(
+            icon: const Image(image: AssetImage(tGoogleLogoImage), width: 20.0),
+            onPressed: () {},
+            label: const Text(tSignInWithGoogle),
+          ),
+        ),
+        const SizedBox(height: tFormHeight - 20),
+        TextButton(
+          onPressed: () {},
+          child: Text.rich(
+            TextSpan(
+                text: tDontHaveAnAccount,
+                style: Theme.of(context).textTheme.bodyText1,
+                children: const [
+                  TextSpan(text: tSignup, style: TextStyle(color: Colors.blue))
+                ]),
+          ),
+        ),
+      ],
+    );
+  }
+}
