63e87e3d988d10972f4e7dce5d9c6d77e6e93bd1
benweru <ben.maina@strathmore.edu>
2024-05-25 01:03:24 +0300

    User repository for data storage into firestore

--- bus_app/lib/src/common_widgets/form/signup_form_widget.dart
+++ bus_app/lib/src/common_widgets/form/signup_form_widget.dart
@@ -1,11 +1,10 @@
+import 'package:bus_app/src/features/authentication/models/user_model.dart';
 import 'package:bus_app/src/features/authentication/screens/forget_password/forget_password_otp/otp_screen.dart';
 import "package:flutter/material.dart";
 import 'package:bus_app/src/constants/sizes.dart';
 import 'package:bus_app/src/constants/text_strings.dart';
 import 'package:get/get.dart';
 import 'package:bus_app/src/features/authentication/controllers/signup_controller.dart';
-
-
 
 class SignUpFormWidget extends StatelessWidget {
   const SignUpFormWidget({
@@ -17,7 +16,6 @@ class SignUpFormWidget extends StatelessWidget {
     final controller = Get.put(SignUpController());
     final _formKey = GlobalKey<FormState>();
 
-
     return Container(
       padding: const EdgeInsets.symmetric(vertical: tFormHeight - 10),
       child: Form(
@@ -27,32 +25,47 @@ class SignUpFormWidget extends StatelessWidget {
           children: [
             TextFormField(
               controller: controller.fullName,
-              decoration: const InputDecoration(label: Text(tFullName), prefixIcon: Icon(Icons.person_outline_rounded)),
+              decoration: const InputDecoration(
+                  label: Text(tFullName),
+                  prefixIcon: Icon(Icons.person_outline_rounded)),
             ),
             const SizedBox(height: tFormHeight - 20),
             TextFormField(
               controller: controller.email,
-              decoration: const InputDecoration(label: Text(tEmail), prefixIcon: Icon(Icons.email_outlined)),
+              decoration: const InputDecoration(
+                  label: Text(tEmail), prefixIcon: Icon(Icons.email_outlined)),
             ),
             const SizedBox(height: tFormHeight - 20),
             TextFormField(
               controller: controller.phoneNo,
-              decoration: const InputDecoration(label: Text(tPhoneNumber), prefixIcon: Icon(Icons.numbers)),
+              decoration: const InputDecoration(
+                  label: Text(tPhoneNumber), prefixIcon: Icon(Icons.numbers)),
             ),
             const SizedBox(height: tFormHeight - 20),
             TextFormField(
               controller: controller.password,
-              decoration: const InputDecoration(label: Text(tPassword), prefixIcon: Icon(Icons.fingerprint)),
+              decoration: const InputDecoration(
+                  label: Text(tPassword), prefixIcon: Icon(Icons.fingerprint)),
             ),
             const SizedBox(height: tFormHeight - 10),
             SizedBox(
               width: double.infinity,
               child: ElevatedButton(
                 onPressed: () {
-                  if(_formKey.currentState!.validate()){
+                  if (_formKey.currentState!.validate()) {
                     // SignUpController.instance.registerUser(controller.email.text.trim(), controller.password.text.trim());
-                    SignUpController.instance.phoneAuthentication(controller.phoneNo.text.trim());
-                    Get.to(() => const OTPScreen());
+
+                    final user = UserModel(
+                      email: controller.email.text.trim(),
+                      password: controller.password.text.trim(),
+                      fullName: controller.fullName.text.trim(),
+                      phoneNo: controller.phoneNo.text.trim(),
+                    );
+
+                    //SignUpController.instance.phoneAuthentication(controller.phoneNo.text.trim());
+                    //Get.to(() => const OTPScreen());
+
+                    SignUpController.instance.createUser(user);
                   }
                 },
                 child: Text(tSignUp.toUpperCase()),
--- bus_app/lib/src/features/authentication/controllers/signup_controller.dart
+++ bus_app/lib/src/features/authentication/controllers/signup_controller.dart
@@ -1,6 +1,8 @@
+import 'package:bus_app/src/features/authentication/models/user_model.dart';
+import 'package:bus_app/src/features/authentication/screens/forget_password/forget_password_otp/otp_screen.dart';
+import 'package:bus_app/src/repository/user_repository/user_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
 import '../../../repository/authentication_repository/authentication_repository.dart';
 
 class SignUpController extends GetxController {
@@ -11,6 +13,7 @@ class SignUpController extends GetxController {
   final password = TextEditingController();
   final fullName = TextEditingController();
   final phoneNo = TextEditingController();
+  final userRepo = Get.put(UserRepository());
 
   //Call this Function from Design & it will do the rest
   void registerUser(String email, String password) {
@@ -21,6 +24,13 @@ class SignUpController extends GetxController {
         message: error.toString(),
       ));
     }
+  }
+
+  //create user into Firebase Database
+  Future<void> createUser(UserModel user) async {
+    await userRepo.createUser(user);
+    phoneAuthentication(user.phoneNo);
+    Get.to(() => const OTPScreen());
   }
 
   //Get phoneNo from user (Screen) and pass it to Auth Repository for Firebase Authentication
